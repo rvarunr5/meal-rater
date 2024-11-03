127994f60ed183cd3252ce972bb5ecb564bc3b8e
Varun <rvarunr5@gmail.com>
2024-11-03 18:06:31 -0500

    Initial Commit

---
+++ MealRater/MealRaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealRaterApp.swift
+//  MealRater
+//
+//  Created by Varun Reddy Regalla on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MealRaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
